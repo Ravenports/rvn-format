2443c034d8d2f96efaa5b2c7f99f4f8a949cd66e
jrmarino <draco@marino.st>
2023-05-21 16:00:47 -0500

    fix trailing argument harder

--- programs/src/xrvn.adb
+++ programs/src/xrvn.adb
@@ -35,86 +35,91 @@ is
          exit when argx > CLI.Argument_Count;
          declare
             this_arg : String renames CLI.Argument (argx);
+            continue : Boolean := False;
          begin
             case next_parameter is
-               when waiting  => null;
+               when waiting  => continue := True;
                when extract  => arg_extract  := ASU.To_Unbounded_String (this_arg);
                when metadata => arg_metadata := ASU.To_Unbounded_String (this_arg);
                when manifest => arg_manifest := ASU.To_Unbounded_String (this_arg);
                when outdir   => arg_outdir   := ASU.To_Unbounded_String (this_arg);
             end case;
             next_parameter := waiting;
-            if this_arg'Length > 1 then
-               if this_arg (this_arg'First .. this_arg'First + 1) = "--" then
-                  if this_arg = "--extract" then
-                     opt_extract := True;
-                     next_parameter := extract;
-                  elsif this_arg = "--metadata" then
-                     opt_metadata := True;
-                     next_parameter := metadata;
-                  elsif this_arg = "--list-manifest" then
-                     opt_manifest := True;
-                     next_parameter := manifest;
-                  elsif this_arg = "--out-dir" then
-                     opt_outdir := True;
-                     next_parameter := outdir;
-                  end if;
-               elsif this_arg (this_arg'First) = '-' then
-                  declare
-                     single : Natural := this_arg'First;
-                  begin
-                     loop
-                        single := single + 1;
-                        exit when single > this_arg'Last;
-                        case this_arg (single) is
-                           when 'v' => opt_verbose := True;
-                           when 'd' => opt_digest := True;
-                           when 'x' | 'm' | 'l' | 'o' =>
-                              case this_arg (single) is
-                                 when 'x' => opt_extract := True;
-                                 when 'm' => opt_metadata := True;
-                                 when 'l' => opt_manifest := True;
-                                 when 'o' => opt_outdir := True;
-                                 when others => null;
-                              end case;
-                              if single = this_arg'Last then
+            if continue then
+               if this_arg'Length > 1 then
+                  if this_arg (this_arg'First .. this_arg'First + 1) = "--" then
+                     if this_arg = "--extract" then
+                        opt_extract := True;
+                        next_parameter := extract;
+                     elsif this_arg = "--metadata" then
+                        opt_metadata := True;
+                        next_parameter := metadata;
+                     elsif this_arg = "--list-manifest" then
+                        opt_manifest := True;
+                        next_parameter := manifest;
+                     elsif this_arg = "--out-dir" then
+                        opt_outdir := True;
+                        next_parameter := outdir;
+                     end if;
+                  elsif this_arg (this_arg'First) = '-' then
+                     declare
+                        single : Natural := this_arg'First;
+                     begin
+                        loop
+                           single := single + 1;
+                           exit when single > this_arg'Last;
+                           case this_arg (single) is
+                              when 'v' => opt_verbose := True;
+                              when 'd' => opt_digest := True;
+                              when 'x' | 'm' | 'l' | 'o' =>
                                  case this_arg (single) is
-                                    when 'x' => next_parameter := extract;
-                                    when 'm' => next_parameter := metadata;
-                                    when 'l' => next_parameter := manifest;
-                                    when 'o' => next_parameter := outdir;
+                                    when 'x' => opt_extract := True;
+                                    when 'm' => opt_metadata := True;
+                                    when 'l' => opt_manifest := True;
+                                    when 'o' => opt_outdir := True;
                                     when others => null;
                                  end case;
-                              else
-                                 declare
-                                    remainder : constant text := ASU.To_Unbounded_String
-                                      (this_arg (single + 1 .. this_arg'Last));
-                                 begin
+                                 if single = this_arg'Last then
                                     case this_arg (single) is
+                                       when 'x' => next_parameter := extract;
+                                       when 'm' => next_parameter := metadata;
+                                       when 'l' => next_parameter := manifest;
+                                       when 'o' => next_parameter := outdir;
+                                       when others => null;
+                                    end case;
+                                 else
+                                    declare
+                                       remainder : constant text := ASU.To_Unbounded_String
+                                         (this_arg (single + 1 .. this_arg'Last));
+                                    begin
+                                       case this_arg (single) is
                                        when 'x' => arg_extract  := remainder;
                                        when 'm' => arg_metadata := remainder;
                                        when 'l' => arg_manifest := remainder;
                                        when 'o' => arg_outdir   := remainder;
                                        when others => null;
-                                    end case;
-                                 end;
-                                 single := this_arg'Last;
-                              end if;
-                           when others => null;
-                        end case;
-                     end loop;
-                  end;
-               else
-                  if next_parameter = waiting then
+                                       end case;
+                                    end;
+                                    single := this_arg'Last;
+                                 end if;
+                              when others => null;
+                           end case;
+                        end loop;
+                     end;
+                  else
                      if not filename_set then
                         arg_filename := ASU.To_Unbounded_String (this_arg);
                         filename_set := True;
                      end if;
                      --  Subsequent times here will drop the argument
                   end if;
+               else
+                  --  single character argument
+                  if not filename_set then
+                     arg_filename := ASU.To_Unbounded_String (this_arg);
+                     filename_set := True;
+                  end if;
                end if;
-            else
-               null;   -- TODO something
             end if;
          end;
       end loop;
