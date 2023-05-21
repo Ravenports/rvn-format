267c7a21ab46e7d21f33af22cbc72d53e11a8360
jrmarino <draco@marino.st>
2023-05-21 15:40:52 -0500

    reimplement getopt parser

--- programs/src/xrvn.adb
+++ programs/src/xrvn.adb
@@ -1,34 +1,131 @@
-with Getopts;
 with Ada.Text_IO;
+with Ada.Strings.Unbounded;
+with Ada.Command_Line;
 
 procedure Xrvn
 is
    package TIO renames Ada.Text_IO;
+   package ASU renames Ada.Strings.Unbounded;
+   package CLI renames Ada.Command_Line;
 
-   gparser : Getopts.Parser;
+   subtype text is ASU.Unbounded_String;
+   procedure process_arguments;
+
+   opt_verbose  : Boolean := False;
+   opt_digest   : Boolean := False;
+   opt_extract  : Boolean := False;
+   opt_metadata : Boolean := False;
+   opt_manifest : Boolean := False;
+   opt_outdir   : Boolean := False;
+   arg_extract  : text;
+   arg_metadata : text;
+   arg_manifest : text;
+   arg_outdir   : text;
+   arg_filename : text;
+
+   procedure process_arguments
+   is
+      type compound is (waiting, extract, metadata, manifest, outdir);
+      argx : Natural := 0;
+      next_parameter : compound := waiting;
+      filename_set : Boolean := False;
+   begin
+      loop
+         argx := argx + 1;
+         exit when argx > CLI.Argument_Count;
+         declare
+            this_arg : String renames CLI.Argument (argx);
+         begin
+            case next_parameter is
+               when waiting  => null;
+               when extract  => arg_extract  := ASU.To_Unbounded_String (this_arg);
+               when metadata => arg_metadata := ASU.To_Unbounded_String (this_arg);
+               when manifest => arg_manifest := ASU.To_Unbounded_String (this_arg);
+               when outdir   => arg_outdir   := ASU.To_Unbounded_String (this_arg);
+            end case;
+            next_parameter := waiting;
+            if this_arg'Length > 1 then
+               if this_arg (this_arg'First .. this_arg'First + 1) = "--" then
+                  if this_arg = "--extract" then
+                     opt_extract := True;
+                     next_parameter := extract;
+                  elsif this_arg = "--metadata" then
+                     opt_metadata := True;
+                     next_parameter := metadata;
+                  elsif this_arg = "--list-manifest" then
+                     opt_manifest := True;
+                     next_parameter := manifest;
+                  elsif this_arg = "--out-dir" then
+                     opt_outdir := True;
+                     next_parameter := outdir;
+                  end if;
+               elsif this_arg (this_arg'First) = '-' then
+                  declare
+                     single : Natural := this_arg'First;
+                  begin
+                     loop
+                        single := single + 1;
+                        exit when single > this_arg'Last;
+                        case this_arg (single) is
+                           when 'v' => opt_verbose := True;
+                           when 'd' => opt_digest := True;
+                           when 'x' | 'm' | 'l' | 'o' =>
+                              case this_arg (single) is
+                                 when 'x' => opt_extract := True;
+                                 when 'm' => opt_metadata := True;
+                                 when 'l' => opt_manifest := True;
+                                 when 'o' => opt_outdir := True;
+                                 when others => null;
+                              end case;
+                              if single = this_arg'Last then
+                                 case this_arg (single) is
+                                    when 'x' => next_parameter := extract;
+                                    when 'm' => next_parameter := metadata;
+                                    when 'l' => next_parameter := manifest;
+                                    when 'o' => next_parameter := outdir;
+                                    when others => null;
+                                 end case;
+                              else
+                                 declare
+                                    remainder : constant text := ASU.To_Unbounded_String
+                                      (this_arg (single + 1 .. this_arg'Last));
+                                 begin
+                                    case this_arg (single) is
+                                       when 'x' => arg_extract  := remainder;
+                                       when 'm' => arg_metadata := remainder;
+                                       when 'l' => arg_manifest := remainder;
+                                       when 'o' => arg_outdir   := remainder;
+                                       when others => null;
+                                    end case;
+                                 end;
+                                 single := this_arg'Last;
+                              end if;
+                           when others => null;
+                        end case;
+                     end loop;
+                  end;
+               else
+                  if not filename_set then
+                     arg_filename := ASU.To_Unbounded_String (this_arg);
+                     filename_set := True;
+                  end if;
+                  --  Subsequent times here will drop the argument
+               end if;
+            else
+               null;   -- TODO something
+            end if;
+         end;
+      end loop;
+
+   end process_arguments;
+
 begin
-   for switch in gparser.Getopt(":vdx:m:l:o:") loop
-      case Getopts.Option (switch) is
-         when 'v' => TIO.Put_Line ("verbose [short]");
-         when 'd' => TIO.Put_Line ("digest [short]");
-         when 'x' =>
-            TIO.Put_Line ("extract [short]");
-            TIO.Put_Line ("follows: " & Getopts.Optarg (switch));
-         when 'm' =>
-            TIO.Put_Line ("metadata [short]");
-            TIO.Put_Line ("follows: " & Getopts.Optarg (switch));
-         when 'l' =>
-            TIO.Put_Line ("list-manifest [short]");
-            TIO.Put_Line ("follows: " & Getopts.Optarg (switch));
-         when 'o' =>
-            TIO.Put_Line ("outdir [short]");
-            TIO.Put_Line ("follows: " & Getopts.Optarg (switch));
-         when others =>
-            TIO.Put_Line ("not handled: " & Getopts.option (switch));
-      end case;
-   end loop;
-
-   	for I in 1 .. gparser.Argument_Count loop
-		TIO.Put_Line ("Argument " & I'Image & ": " & gparser.Argument(I));
-	end loop;
+   process_arguments;
+   TIO.Put_Line ("v: " & opt_verbose'Img);
+   TIO.Put_Line ("d: " & opt_digest'Img);
+   TIO.Put_Line ("x: " & opt_extract'Img & "   [" & ASU.To_String (arg_extract) & "]");
+   TIO.Put_Line ("m: " & opt_metadata'Img & "   [" & ASU.To_String (arg_metadata) & "]");
+   TIO.Put_Line ("l: " & opt_manifest'Img & "   [" & ASU.To_String (arg_manifest) & "]");
+   TIO.Put_Line ("o: " & opt_outdir'Img & "   [" & ASU.To_String (arg_outdir) & "]");
+   TIO.Put_Line ("filename: " & ASU.To_String (arg_filename));
 end Xrvn;
